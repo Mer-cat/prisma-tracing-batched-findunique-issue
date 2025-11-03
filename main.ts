f5ca90b3d1a98cd21ea735e8a06daaa74875a0b3
Mercy Bickell <mbickell@lightboxre.com>
2025-11-03 11:18:58 -0700

    Add postgresdb to docker

--- main.ts
+++ main.ts
@@ -35,7 +35,9 @@ registerInstrumentations({
 provider.register();
 
 const prisma = new PrismaClient({
-  adapter: new PrismaPg({ connectionString: `${process.env.DATABASE_URL}` })
+  adapter: new PrismaPg({
+    connectionString: process.env.DATABASE_URL
+  })
 });
 
 async function main() {
